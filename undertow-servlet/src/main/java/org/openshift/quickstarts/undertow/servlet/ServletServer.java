1818cfe9078e5fda8231bf7643fe529c34c007f2
je-MBPR <>
2018-11-09 18:27:05 +0800

    test

--- undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
+++ undertow-servlet/src/main/java/org/openshift/quickstarts/undertow/servlet/ServletServer.java
@@ -54,7 +54,7 @@ public class ServletServer {
                     .setDeploymentName("test.war")
                     .addServlets(
                             servlet("MessageServlet", MessageServlet.class)
-                                    .addInitParam("message", "Hello Singing")
+                                    .addInitParam("message", "Hello HCP")
                                     .addMapping("/*"),
                             servlet("MyServlet", MessageServlet.class)
                                     .addInitParam("message", "MyServlet")
